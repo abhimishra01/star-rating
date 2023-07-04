f676edf858ee29fd57e1f8ef4e5d32dcb541363d
Abhishek <abhimishraa@Abhisheks-MacBook-Pro.local>
2023-07-05 01:01:03 +0530

    feat: Star Rating component

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Rating from "./components/Rating";
+
+function App() {
+  return (
+    <>
+      <h1> Rate this App ... </h1>
+      <Rating />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Rating/index.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import "./styles.css";
+
+export default function Rating(props) {
+  const [rating, setRating] = useState(0);
+  const [hover, setHover] = useState(false);
+
+  return (
+    <div className="star-rating">
+      {[...Array(5)].map((star, index) => {
+        index += 1;
+        return (
+          <button
+            type="button"
+            key={index}
+            className={index <= (hover || rating) ? "on" : "off"}
+            onClick={() => setRating(index)}
+            onMouseEnter={() => setHover(index)}
+            onMouseLeave={() => setHover(rating)}
+          >
+            <span className="star">&#9733;</span>
+          </button>
+        );
+      })}
+    </div>
+  );
+}
